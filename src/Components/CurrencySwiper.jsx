488818f15ab78460d6b4ca4dee1ab55cfe5ef1fb
francooparodi01 <francoparodi2001@gmail.com>
2024-11-05 18:23:38 -0300

    cambios en estilos

--- src/Components/CurrencySwiper.jsx
+++ src/Components/CurrencySwiper.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 import CurrencyCard from './CurrencyCard';
 import 'swiper/swiper-bundle.css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css';
 import { ApiContext } from '../services/ApiContext';
-import { Box } from '@mui/material'; 
+import { Box, Typography, Container } from '@mui/material';
 
 const CurrencySwiper = () => {
     const { data, loading, error } = useContext(ApiContext);
@@ -14,41 +15,45 @@ const CurrencySwiper = () => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
     if (!data || data.length === 0) return <p>No hay datos disponibles</p>;
-    
-    const titles = data.map((item) => item.casa);
-    const compras = data.map((item) => item.compra);
-    const ventas = data.map((item) => item.venta);
+
     const lastUpdates = data.map((item) => new Date(item.fechaActualizacion).toLocaleString());
 
     return (
-        <Box sx={{ padding: 2 }}> 
-            <Swiper
-                spaceBetween={20}
-                navigation
-                breakpoints={{
-                    640: {
-                        slidesPerView: 1, 
-                    },
-                    768: {
-                        slidesPerView: 2,
-                    },
-                    1024: {
-                        slidesPerView: 3, 
-                    },
-                }}
-            >
-                {data.map((currency, index) => (
-                    <SwiperSlide key={currency.casa}>
-                        <CurrencyCard
-                            title={currency.casa} 
-                            venta={currency.venta}
-                            compra={currency.compra}
-                            lastUpdate={lastUpdates[index]}  
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </Box>
+        <Container sx={{ padding: 2 }}>
+            <Typography variant="h5" component="h2" sx={{ marginBottom: 2 }}>
+                Cotizaciones de Divisas
+            </Typography>
+                <Swiper
+                    spaceBetween={20}
+                    autoplay={{ delay: 1500, disableOnInteraction: false }}
+                    modules={[Autoplay]}
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 1,
+                        },
+                        480: {
+                            slidesPerView: 2,
+                        },
+                        768: {
+                            slidesPerView: 3,
+                        },
+                        1024: {
+                            slidesPerView: 4,
+                        },
+                    }}
+                >
+                    {data.map((currency, index) => (
+                        <SwiperSlide key={currency.casa}>
+                            <CurrencyCard
+                                title={currency.casa} 
+                                venta={currency.venta}
+                                compra={currency.compra}
+                                lastUpdate={lastUpdates[index]}  
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+        </Container>
     );
 };
 

eb4404a69662d5526b9b5fc3c7f6fec497d58b0c
Francoparodi01 <francoparodi2001@gmail.com>
2024-11-27 10:05:58 -0300

    Agregando charts para infla + datos históricos

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ const App = () => {
                     <CurrencySwiper />
                     <CurrencyList />
                     <InflationChart />
+                    <p> Para ver algiito </p>
                   </>
                 }
               />
